0ea663afb8f2b0e2325f657550d5a766bace21c7
Praz Wal <hayden@Prajwals-MacBook-Pro.local>
2018-01-13 20:30:37 +0545

    at the expert tutorial

---
+++ mnist_deep.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+#from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.learn.python.learn.datasets.mnist import load_mnist
+
+mnist = load_mnist()
+
+
